08f5edf0790ce39613c7d4cac94cdc70dd245078
Ismail Mohammed <isse_a@hotmail.com>
2026-02-20 15:34:31 +0100

    model

---
+++ LAB_NN/utility_functions.py
@@ -0,0 +1,17 @@
+import wfdb
+import numpy as np
+
+def load_raw_data(df, sampling_rate, path):
+    if sampling_rate == 100:
+        data = [wfdb.rdsamp(path+f) for f in df.filename_lr]
+    else:
+        data = [wfdb.rdsamp(path+f) for f in df.filename_hr]
+    data = np.array([signal for signal, meta in data])
+    return data
+
+def aggregate_diagnostic(y_dic, agg_df):
+    tmp = []
+    for key in y_dic.keys():
+        if key in agg_df.index:
+            tmp.append(agg_df.loc[key].diagnostic_subclass)
+    return list(set(tmp))
